00743157de0647d84c308300c9fa8d318cf3339e
jacobmaniscalco <jacobmaniscalco9@gmail.com>
2024-09-17 05:22:37 -0400

    Updated flags to include new functionality

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,60 +1,100 @@
-/*
-
-Copyright © 2024 Jacob Maniscalco
-*/
 package cmd
 
 import (
 	"fmt"
-	
-	"github.com/spf13/cobra"
-	"github.com/jacobmaniscalco/blue-caterpillar-cli/internal/scan"
 
+	"github.com/charmbracelet/bubbletea"
+	"github.com/jacobmaniscalco/blue-caterpillar-cli/internal/scan"
+	"github.com/jacobmaniscalco/blue-caterpillar-cli/ui/components"
+	"github.com/spf13/cobra"
 )
 
-var target string 
-var ports string
-var verbose bool
+var options scan.ScanOptions
 
 // scanCmd represents the scan command
 var ScanCmd = &cobra.Command{
 	Use:   "scan",
 	Short: "Perform a network scan to detect vulnerabilities using Nmap.",
-	Long: 
-`Scan the specified targets for open ports, services, and potential vulnerabilities using the Nmap library.
-This command leverages the power of Nmap, a widely used network scanning tool, to perform comprehensive scans on one or more targets.
-The scan can detect open ports, identify running services, and perform OS detection, among other features. 
-Use this command to gather detailed information about the network topology and identify potential security weaknesses in your environment.
-Examples:
-  	- Scan a single IP address:
-      	scan 192.168.1.1
-  	- Scan a range of IP addresses:
-      	scan 192.168.1.1-10
-  	- Scan a specific domain:
-      	scan example.com
-  	- Scan using OS detection:
-      	scan --os-detection example.com`,
+	Long: `Scan the specified targets for open ports, services, and 
+potential vulnerabilities using the Nmap library.This command leverages the power
+of Nmap, a widely used network scanning tool, to perform comprehensive scans on
+one or more targets.The scan can detect open ports,identify running services, 
+and perform OS detection, among other features. Use this command to gather 
+detailed information about the network topology and identify potential security
+weaknesses in your environment.`,
 
-	Run: func(cmd *cobra.Command, args []string) {	
+	Run: func(cmd *cobra.Command, args []string) {
 
-		_, err := scan.RunScan(target, ports)
+		p := tea.NewProgram(components.NewModel(options))
+
+		finalModel, err := p.Run()
 
 		if err != nil {
-			fmt.Printf("Error running scan: %v\n", err)
-			return
+			fmt.Println("Error with bubbletea rendering: ", err)
+		}
+
+		if finalModel, ok := finalModel.(components.ScannerModel); ok {
+
+			if finalModel.Err != nil {
+				fmt.Println("Error running scan: ", finalModel.Err)
+			}
+
+			if finalModel.Result != "" {
+				fmt.Println(finalModel.Result)
+			}
 		}
 	},
 }
 
 func init() {
 
-	// flag for IP address to scan
-	ScanCmd.Flags().StringVarP(&target, "target", "t", "", "Target to scan")
+	
+	ScanCmd.Flags().StringVarP(&options.Target, "target", "t", "",
+`Specify the target IP address or range of IP addresses to be scanned. 
+This can be a single IP, a subnet, or a list of IPs.
+`)
 	ScanCmd.MarkFlagRequired("target")
-        
-	// flag for ports to scan
-	ScanCmd.Flags().StringVarP(&ports, "ports", "p", "", "Ports to scan")
 
-	// flag for verbose output
-	ScanCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose output")
+	ScanCmd.Flags().StringVarP(&options.Ports, "ports", "p", "", 
+`Define which ports to scan on the target. You can specify a single port, a range of ports (e.g., 1-1024),
+or multiple ports separated by commas.
+`)
+
+	ScanCmd.Flags().StringVarP(&options.Script, "script", "s", "",
+`Indicate the Nmap scripts to be used during the scan.
+You can provide a specific script or a category of scripts (e.g., default, vuln).
+`)
+
+	ScanCmd.Flags().StringVarP(&options.Timing, "timing", "", "",
+`Set the timing template for the scan, which controls the speed
+and stealthiness of the scan. Timing templates range from 0 
+(paranoid) to 5 (insane), with higher numbers increasing speed
+but also the risk of detection.
+`)
+
+	ScanCmd.Flags().BoolVar(&options.SkipHostDiscovery, "skip-host-discovery",false,
+`Disable host discovery phase, which skips the process of
+identifying live hosts and assumes that the targets are up.
+`)
+
+	ScanCmd.Flags().BoolVarP(&options.Aggressive, "aggressive", "a", false,
+`Enable aggressive scan options, which include service detection, 
+OS detection, and additional scanning techniques to gather more information.
+`)
+
+	ScanCmd.Flags().BoolVar(&options.ServiceDetection, "service-detection", false,
+`Enable detection of services running on the open 
+ports of the target.This helps in identifying the applications
+and versions running on the target.
+`)
+
+	ScanCmd.Flags().BoolVar(&options.OsDetection, "os-detection", false,
+`Enable OS detection to determine the operating system
+of the target host. This uses various techniques to guess the 
+OS based on network responses.
+`)
+
+	ScanCmd.Flags().StringVar(&options.ScanType, "scan-type", "", 
+`Specify scan type (e.g., sS for SYN, sT for TCP connect, sU for UDP)
+`)
 }
