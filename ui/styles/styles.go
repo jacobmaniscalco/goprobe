613708e48d8b874e35f73f5032eb8711f5099a1f
jacobmaniscalco <jacobmaniscalco9@gmail.com>
2024-11-11 15:49:01 -0500

    Updated code formatting

--- ui/styles/styles.go
+++ ui/styles/styles.go
@@ -2,46 +2,41 @@ package styles
 
 import "github.com/charmbracelet/lipgloss"
 
-
 var WarningStyle = lipgloss.NewStyle().
-		   Bold(true).
-		   Foreground(lipgloss.Color("160"))
+	Bold(true).
+	Foreground(lipgloss.Color("160"))
 
 var SuccessStyle = lipgloss.NewStyle().
-		   Foreground(lipgloss.Color("#00ff00"))
+	Foreground(lipgloss.Color("#00ff00"))
 
 var FailStyle = lipgloss.NewStyle().
-		Foreground(lipgloss.Color("9"))
+	Foreground(lipgloss.Color("9"))
 
 var IPStyle = lipgloss.NewStyle().
-              Foreground(lipgloss.Color("129")).
-	      Padding(0, 1)
-
+	Foreground(lipgloss.Color("129")).
+	Padding(0, 1)
 
 var OSStyle = lipgloss.NewStyle().
-	      Italic(true).
-	      Foreground(lipgloss.Color("23")).
-	      Padding(0, 1)
+	Italic(true).
+	Foreground(lipgloss.Color("23")).
+	Padding(0, 1)
 
 var HeaderStyle = lipgloss.NewStyle().
-		  Bold(true).
-		  Foreground(lipgloss.Color("12")).
-		  Background(lipgloss.Color("0")).
-		  Padding(0, 1)
+	Bold(true).
+	Foreground(lipgloss.Color("12")).
+	Background(lipgloss.Color("0")).
+	Padding(0, 1)
 
 var EvenRowStyle = lipgloss.NewStyle().
-		   Background(lipgloss.Color("8")).
-		   Padding(0,1)
+	Background(lipgloss.Color("8")).
+	Padding(0, 1)
 
 var OddRowStyle = lipgloss.NewStyle().
-		  Background(lipgloss.Color("0")).
-		  Padding(0,1)
+	Background(lipgloss.Color("0")).
+	Padding(0, 1)
 
 var TitleStyle = lipgloss.NewStyle().
-		 Bold(true).
-		 Foreground(lipgloss.Color("3")).
-		 Background(lipgloss.Color("black")).
-		 Align(lipgloss.Center)
-		 
-
-
+	Bold(true).
+	Foreground(lipgloss.Color("3")).
+	Background(lipgloss.Color("black")).
+	Align(lipgloss.Center)
