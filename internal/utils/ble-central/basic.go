73b917b7430390fe5a15a6bf5163d5c46b17b08a
jacobmaniscalco <jacobmaniscalco9@gmail.com>
2024-11-24 17:18:37 -0500

    Updated logic for simple ble-central advertising and connection

--- internal/utils/ble-central/basic.go
+++ internal/utils/ble-central/basic.go
@@ -3,18 +3,17 @@ package main
 import (
 	"fmt"
 	"tinygo.org/x/bluetooth"
-//	"time"
+	"time"
 )
 
 var adapter = bluetooth.DefaultAdapter
 
 func main() {
 
-	// Enable BLE interface.
 	must("enable BLE stack", adapter.Enable())
 
-	// Start scanning.
 	fmt.Println("scanning...")
+	// Scan for a device with local name: Go Bluetooth and connect
 	err := adapter.Scan(func(adapter *bluetooth.Adapter, device bluetooth.ScanResult) {
 
 		if device.LocalName() == "Go Bluetooth" {
@@ -28,17 +27,23 @@ func main() {
 
 func connectToDevice(adapter *bluetooth.Adapter, address bluetooth.Address) {
 
+
+	_, err := adapter.Connect(address, bluetooth.ConnectionParams{})
+	if err == nil {
+		fmt.Println("Connected Successfully")
+	}
+	time.Sleep(2 * time.Second)
+	
 	device, err := adapter.Connect(address, bluetooth.ConnectionParams{})
 	if err != nil {
 		fmt.Printf("Error connecting to device: %v\n", err)
 	}
 	fmt.Printf("Device Address: %v\n", device.Address)
 
-	// ensure connection is stable
-	//time.Sleep(5 * time.Second)
-
-	serviceUUID := bluetooth.UUID([4]uint32{1,2,3,4})
-	characteristicUUID := bluetooth.UUID([4]uint32{5,6,7,8})
+	time.Sleep(5 * time.Second)
+	// service UUID and characteristicUUID defined by the peripheral device
+	serviceUUID := bluetooth.UUID([4]uint32{1, 2, 3, 4})
+	characteristicUUID := bluetooth.UUID([4]uint32{5, 6, 7, 8})
 
 	services, err := device.DiscoverServices([]bluetooth.UUID{serviceUUID})
 	if err != nil {
@@ -51,25 +56,20 @@ func connectToDevice(adapter *bluetooth.Adapter, address bluetooth.Address) {
 
 	for _, service := range services {
 		characteristics, err := service.DiscoverCharacteristics([]bluetooth.UUID{characteristicUUID})
-
 		if err != nil {
 			fmt.Println("Error reading characteristics from service: ", err)
 		}
-
 		for _, characteristic := range characteristics {
 
 			data := make([]byte, 128)
-
 			n, err := characteristic.Read(data)
 
 			if err != nil {
 				fmt.Println("Error reading characteristic: ", err)
 				continue
 			}
-
 			fmt.Printf("Read %d bytes from characteristic: %s\n", n, string(data[:n]))
 		}
-
 	}
 }
 
