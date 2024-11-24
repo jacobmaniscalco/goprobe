9a4bfce3af9d248dcf68895a84e620cd732d496e
jacobmaniscalco <jacobmaniscalco9@gmail.com>
2024-11-24 17:16:40 -0500

    WIP: updated cobra scan command to test ble scanning

--- cmd/scan.go
+++ cmd/scan.go
@@ -1,19 +1,14 @@
 package cmd
 
 import (
-	//"fmt"
-
-	//"github.com/charmbracelet/bubbletea"
 	"github.com/jacobmaniscalco/goprobe/internal/scan"
-	//"github.com/jacobmaniscalco/goprobe/internal/scan/snmp"
-	//"github.com/jacobmaniscalco/goprobe/ui/components"
 	"github.com/spf13/cobra"
 	"github.com/jacobmaniscalco/goprobe/internal/scan/ble"
 )
 
 var scanOptions scan.ScanOptions
 var ports string
-var iface string
+var macAddress string
 
 // scanCmd represents the scan command
 var ScanCmd = &cobra.Command{
@@ -28,73 +23,11 @@ var ScanCmd = &cobra.Command{
               "weaknesses in your environment.",
 
 	Run: func(cmd *cobra.Command, args []string) {
-
-		ble.BLE_scan(iface)
-		
-		//snmp.ScanSNMPDevice(scanOptions, 161)
-
-		//p := tea.NewProgram(components.NewModel(scanOptions))
-
-		//finalModel, err := p.Run()
-
-		//if err != nil {
-		//	fmt.Println("Error with bubbletea rendering: ", err)
-		//}
-
-		//if finalModel, ok := finalModel.(components.ScannerModel); ok {
-
-		//	if finalModel.Err != nil {
-		//		fmt.Println("Error running scan: ", finalModel.Err)
-		//	}
-
-//			if finalModel.Result != "" {
-//				fmt.Println(finalModel.Result)
-//			}
-//		}
+		ble.StartSniffer(macAddress)
 	},
 }
 
 func init() {
 
-	ScanCmd.Flags().StringVarP(&iface, "interface", "i", "", "Specify interface")
-
-	
-	ScanCmd.Flags().StringVarP(&scanOptions.Host, "target", "t", "",
-	"Specify the target IP address or range of IP addresses to be scanned.\n" + 
-	"This can be a single IP, a subnet, or a list of IPs.\n")
-
-	ScanCmd.Flags().StringVarP(&ports, "ports", "p", "", 
-	"Define which ports to scan on the target. You can specify a single port, \n" +
-	"a range of ports (e.g., 1-1024), or multiple ports separated by commas.\n")
-
-	ScanCmd.Flags().StringVarP(&scanOptions.Script, "script", "s", "",
-	"Indicate the Nmap scripts to be used during the scan.\n" +
-	"You can provide a specific script or a category of scripts (e.g., default, vuln).\n")
-
-	ScanCmd.Flags().StringVarP(&scanOptions.Timing, "timing", "", "",
-	"Set the timing template for the scan, which controls the speed\n " +
-	"and stealthiness of the scan. Timing templates range from 0\n " +
-	"(paranoid) to 5 (insane), with higher numbers increasing speed\n " +
-	"but also the risk of detection.\n")
-
-	ScanCmd.Flags().BoolVar(&scanOptions.SkipHostDiscovery, "skip-host-discovery",false,
-	"Disable host discovery phase, which skips the process of \n" + 
-	"identifying live hosts and assumes that the targets are up.\n")
-
-	ScanCmd.Flags().BoolVarP(&scanOptions.Aggressive, "aggressive", "a", false,
-	"Enable aggressive scan options, which include service detection, \n" +
-	"OS detection, and additional scanning techniques to gather more information.\n")
-
-	ScanCmd.Flags().BoolVar(&scanOptions.ServiceDetection, "service-detection", false,
-	"Enable detection of services running on the open \n" + 
-	"ports of the target.This helps in identifying the applications " +
-	"and versions running on the target.\n")
-
-	ScanCmd.Flags().BoolVar(&scanOptions.OsDetection, "os-detection", false,
-	"Enable OS detection to determine the operating system \n" + 
-	"of the target host. This uses various techniques to guess the \n" + 
-	"OS based on network responses.\n")
-
-	ScanCmd.Flags().StringVar(&scanOptions.ScanType, "scan-type", "", 
-	"Specify scan type (e.g., sS for SYN, sT for TCP connect, sU for UDP)\n")
+	ScanCmd.Flags().StringVarP(&macAddress, "macAddress", "a", "", "Specify the MAC address of the device you are attempting to capture.")
 }
